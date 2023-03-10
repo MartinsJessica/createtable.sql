9804be7db9eba20a2fbd2917387d279c0c658642
MartinsJessica <101491038+MartinsJessica@users.noreply.github.com>
2023-03-10 20:47:44 -0300

    Create insert.sql

---
+++ insert.sql
@@ -0,0 +1,3 @@
+INSERT INTO empregado (nome, dt_nasc, area, matricula, ativo)
+    VALUES ('Jessica', 'dev master', '05/11/1999', 139, TRUE),
+    	   ('Maria', 'cantora', '09/10/2002', 122, FALSE);
