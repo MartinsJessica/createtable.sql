a5ade8d0228648b5b8ab66457fa79e48db476d32
MartinsJessica <101491038+MartinsJessica@users.noreply.github.com>
2023-03-10 20:46:16 -0300

    Create createtable.sql

---
+++ createtable.sql
@@ -0,0 +1,12 @@
+CREATE TABLE empregado (
+    id int NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(50),
+    dt_nasc VARCHAR (10),
+    matricula int,
+    ativo boolean,
+    PRIMARY KEY(id)
+);
+
+
+    
+    
