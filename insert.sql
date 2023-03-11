0d52e287081c4f9d9f4ddbe4f2c92715c4741011
MartinsJessica <101491038+MartinsJessica@users.noreply.github.com>
2023-03-10 21:27:05 -0300

    Update insert.sql

--- insert.sql
+++ insert.sql
@@ -1,3 +1,3 @@
 INSERT INTO empregado (nome, dt_nasc, area, matricula, ativo)
-    VALUES ('Jessica', 'dev master', '05/11/1999', 139, TRUE),
-    	   ('Maria', 'cantora', '09/10/2002', 122, FALSE);
+    VALUES ('Jessica', '05/11/1999', 'dev master', 139, TRUE),
+    	   ('Maria', '09/10/2002', 'cantora', 122, FALSE);
